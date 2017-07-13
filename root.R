1d56214736649858c67902f4c56fa097e677da49
Kevin Chang <kcha193@aucklanduni.ac.nz>
2017-07-14 11:46:40 +1200

    delete some files

---
+++ root.R
@@ -0,0 +1,13 @@
+
+
+
+rmarkdown::render("Session 1/Slides/Session1.Rmd")
+
+rmarkdown::render("Session 1/Exercise 1/exercise1.Rmd")
+
+
+rmarkdown::render("Session 2/Slides/Session2.Rmd")
+rmarkdown::render("Session 3/Slides/Session3.Rmd")
+rmarkdown::render("Session 4/Slides/Session4.Rmd")
+
+
